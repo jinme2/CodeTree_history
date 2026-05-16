9c021071a0f50c58ff528ab2b8d034ed39f51a47
choi sanhae <155524668+jinme2@users.noreply.github.com>
2026-05-16 12:48:01 +0900

    [Passed] 원소 10개의 합 (8ms, 0MB)

---
+++ trail1/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arry[10];
+    int sum = 0;
+
+    for(int i = 0; i<10; i++){
+        scanf("%d", &arry[i]);
+        sum += arry[i];
+    }
+
+    printf("%d", sum);
+    return 0;
+}
