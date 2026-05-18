edc6e3841edc5dc65355409905971662eb0288ce
choi sanhae <155524668+jinme2@users.noreply.github.com>
2026-05-18 21:59:34 +0900

    [Passed] 문자열 두번 출력하기 (8ms, 0MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char str[100];
+
+    scanf("%s", str);
+
+    printf("%s\n", str);
+    printf("%s\n", str);
+    return 0;
+}
