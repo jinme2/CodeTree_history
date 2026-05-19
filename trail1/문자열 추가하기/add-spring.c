22657688a5f8e269777c0f6da09ab691a37946fb
choi sanhae <155524668+jinme2@users.noreply.github.com>
2026-05-19 13:05:50 +0900

    [Passed] 문자열 추가하기 (1ms, 0MB)

---
+++ trail1/문자열 추가하기/add-spring.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Please write your code here.
+    char str[21];
+    scanf("%s", str);
+    printf("%s", strcat(str, "Hello"));
+    return 0;
+}
