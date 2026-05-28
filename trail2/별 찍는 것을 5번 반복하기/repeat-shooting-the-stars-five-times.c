3b2fd3d3d20d4bf6e4cc5226af3147c289033a95
choi sanhae <155524668+jinme2@users.noreply.github.com>
2026-05-28 15:33:31 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print(){
+    printf("**********\n**********\n**********\n**********\n**********");
+}
+
+int main() {
+    // Please write your code here.
+    print();
+    return 0;
+}
